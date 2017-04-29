c301f5fc976faad71a32f6cbd51e05bf354f6e2f
y.okudera <mail@sample.com>
2017-04-29 10:37:38 +0900

    [Update] ヒラギノ角ゴシックの文字の下部が切れる問題の対応例を追加

--- ios-uikit-uibutton-demo/ViewController.swift
+++ ios-uikit-uibutton-demo/ViewController.swift
@@ -8,9 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
     @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var hiraginoButton: UIButton!
+    
+    // MARK: - life cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,37 +21,47 @@ class ViewController: UIViewController {
         setupTitleColor()
         getCurrentTitle()
         setupImage()
+        
+        setupVerticalAlignment()
     }
+}
+
+// MARK: - file private
+private extension ViewController {
     
     // 現在表示されているタイトルを取得する
-    private func getCurrentTitle(){
+    func getCurrentTitle(){
         print(button.currentTitle!)
     }
     
     // ボタンのタイトルを設定する
-    private func setupTitle(){
-        button.setTitle("ボタン", for: .normal)
+    func setupTitle(){
+        button.setTitle("ボタン2", for: .normal)
     }
     
     // ボタンのタイトルの色を設定する
-    private func setupTitleColor(){
+    func setupTitleColor(){
         button.setTitleColor(UIColor.red, for: .normal)
     }
     
     // ボタンのタイトルの影を設定する
-    private func setupTitleShadowColor(){
+    func setupTitleShadowColor(){
         button.setTitleShadowColor(UIColor.gray, for: .normal)
         button.titleLabel!.shadowOffset = CGSize(width: 3, height: 3)
-
     }
     
     // ボタンに表示する画像を設定する
-    private func setupImage(){
+    func setupImage(){
         let image = UIImage(named:"apple.png")
         button.setImage(image, for: .normal)
         button.sizeToFit()
         button.center = self.view.center
     }
 
+    /// コンテンツの垂直方向の配置を設定する
+    func setupVerticalAlignment() {
+        // ヒラギノ角ゴシックで「g」「j」などの下部が切れないように.fillを設定する
+        // この設定に加えて、Buttonのheightが十分にあれば「g」「j」などの下部は切れない
+        hiraginoButton.contentVerticalAlignment = .fill
+    }
 }
-
